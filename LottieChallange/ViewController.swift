6ffdbe7e6559adbb4565adb6f2aafc36d9700dca
Gizem Zorlu <gizemzorluu@gmail.com>
2023-08-15 14:04:05 +0300

    Initial Commit

---
+++ LottieChallange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottieChallange
+//
+//  Created by Gizem Zorlu on 15.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
